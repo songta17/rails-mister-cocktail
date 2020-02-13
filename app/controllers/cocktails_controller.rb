a22b87408de4ea87476f4168ca3c9abc780dea1f
vorachith phoubandith <songta17@hotmail.com>
2020-02-13 14:34:34 +0100

    charging the seed

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,2 +1,24 @@
 class CocktailsController < ApplicationController
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.find(params[:id])
+    redirect_to cocktail_path(cocktail)
+  end
+
+  private
+
+  # def params_cocktail
+  #   params.require.permit(params[:id])
+  # end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+puts "Cleaning database..."
+Ingredient.destroy_all
+Cocktail.destroy_all
+Dose.destroy_all
+
 Ingredient.create(name: "Light rum")
 Ingredient.create(name: "Dark rum")
 Ingredient.create(name: "Egg")
@@ -15,3 +20,23 @@ Ingredient.create(name: "Amaretto")
 Ingredient.create(name: "lemon")
 Ingredient.create(name: "ice")
 Ingredient.create(name: "mint leaves")
+
+Cocktail.create(name: "Applejack")
+Cocktail.create(name: "Sweet Vermouth")
+Cocktail.create(name: "Apricot Brandy")
+Cocktail.create(name: "Blended Whiskey")
+Cocktail.create(name: "Lime Juice")
+Cocktail.create(name: "Cranberry juice")
+Cocktail.create(name: "Cider")
+
+Dose.create(cocktail_id: 1, ingredient_id: 1, description: "1 cl")
+Dose.create(cocktail_id: 1, ingredient_id: 3, description: "2 cl")
+Dose.create(cocktail_id: 2, ingredient_id: 2, description: "1 cl")
+Dose.create(cocktail_id: 3, ingredient_id: 3, description: "1 cl")
+Dose.create(cocktail_id: 4, ingredient_id: 4, description: "4 cl")
+Dose.create(cocktail_id: 4, ingredient_id: 2, description: "1 cl")
+Dose.create(cocktail_id: 4, ingredient_id: 5, description: "2 cl")
+Dose.create(cocktail_id: 5, ingredient_id: 5, description: "5 cl")
+Dose.create(cocktail_id: 6, ingredient_id: 6, description: "1 cl")
+Dose.create(cocktail_id: 6, ingredient_id: 3, description: "4 cl")
+Dose.create(cocktail_id: 7, ingredient_id: 7, description: "3 cl")
